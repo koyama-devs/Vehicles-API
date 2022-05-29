a73639422dbd9c8b84c409722f8148b21ba45e81
pr0kk <105685714+pr0kk@users.noreply.github.com>
2022-05-29 13:23:47 +0900

    Convert the Pricing Service to a microservice

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,46 +1,39 @@
 package com.udacity.pricing.domain.price;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String currency;
     private BigDecimal price;
     private Long vehicleId;
 
-    public Price() {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Price(String currency, BigDecimal price, Long vehicleId) {
         this.currency = currency;
         this.price = price;
-        this.vehicleId = vehicleId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Long getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(Long vehicleId) {
         this.vehicleId = vehicleId;
     }
 }
---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (1, 'USD', 1.2, 1);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (2, 'USD', 2.5, 2);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (3, 'USD', 5.25, 3);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (4, 'USD', 10.15, 4);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (5, 'USD', 7.33, 5);
